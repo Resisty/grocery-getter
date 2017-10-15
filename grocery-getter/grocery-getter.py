a728b91efdfd9eaae9c8099b4eb3b3f4ea2e2b5e
Christina Guida <christina.guida@jivesoftware.com>
2017-10-15 09:25:55 -0700

    Ideas for how to start

--- grocery-getter/grocery-getter.py
+++ grocery-getter/grocery-getter.py
@@ -6,5 +6,14 @@ import math
 
 import BeautifulSoup
 
+# get ingredients from http://www.eatingwell.com/article/288729/7-day-vegetarian-meal-plan-1200-calories/
 
+
+# get ingredients from linked recipes
+
+
+# read list and add like ingredients together
+
+
+# output list into a txt file
 
